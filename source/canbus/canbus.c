6e335d844d60cb48978d544233534a5b04f84cc9
jomik <lilla_molnet@gmail.com>
2020-09-17 21:34:00 +0200

    Skeleton work

--- source/canbus/canbus.c
+++ source/canbus/canbus.c
@@ -1,3 +1,10 @@
-void can_nop(void){
-    return;
-}
+/**
+ * @file canbus.c
+ * @author jomik (jon.mihkkal.inga@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-17
+ * 
+ * (c) 2020
+ * 
+ */
---
+++ source/canbus/canbus.h
@@ -0,0 +1,17 @@
+/**
+ * @file canbus.h
+ * @author jomik (jon.mihkkal.inga@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-09-17
+ * 
+ * (c) 2020
+ * 
+ */
+
+#ifndef __CANBUS_H__
+#define __CANBUS_H__
+
+
+
+#endif // !__CANBUS_H__
